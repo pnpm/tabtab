0bdb85a7d18ccd7a4a0a41eec4b5cfaf446e6979
khai96_ <hvksmr1996@gmail.com>
2024-01-30 22:39:47 +0700

    test: getCompletionScript

---
+++ test/getCompletionScript.js
@@ -0,0 +1,21 @@
+const assert = require('assert');
+const path = require('path');
+const fs = require('fs');
+const { getCompletionScript } = require('../lib/installer');
+
+describe('getCompletionScript gets the right completion script for', () => {
+  for (const shell of ['bash', 'fish', 'zsh']) {
+    it(shell, async () => {
+      const received = await getCompletionScript({
+        name: 'foo',
+        completer: 'foo-complete',
+        shell
+      });
+      const expected = fs.readFileSync(require.resolve(`../lib/scripts/${shell}.sh`), 'utf8')
+        .replace(/\{pkgname\}/g, 'foo')
+        .replace(/{completer}/g, 'foo-complete')
+        .replace(/\r?\n/g, '\n');
+      assert.equal(received, expected);
+    });
+  }
+});
