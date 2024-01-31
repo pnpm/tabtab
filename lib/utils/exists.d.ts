54a094f2c906a673c9cc91b6d61a00ee98668bd3
khai96_ <hvksmr1996@gmail.com>
2024-01-31 20:38:33 +0700

    chore: remove declaration files

--- lib/utils/exists.d.ts
+++
@@ -1,2 +0,0 @@
-declare function _exports(file: any): Promise<boolean>;
-export = _exports;
--- lib/utils/index.d.ts
+++
@@ -1,4 +0,0 @@
-import systemShell = require("./systemShell");
-import tabtabDebug = require("./tabtabDebug");
-import exists = require("./exists");
-export { systemShell, tabtabDebug, exists };
